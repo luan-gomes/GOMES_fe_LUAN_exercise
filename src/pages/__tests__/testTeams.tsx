0dec3b2bd69995de2b8eb82d23da52a36f6d01c8
Luan Gomes <luangms@outlook.com>
2024-03-09 14:19:01 -0300

    Adding test case and removing unused imports

--- src/pages/__tests__/testTeams.tsx
+++ src/pages/__tests__/testTeams.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {fireEvent, render, screen, waitFor, act} from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import * as API from '../../api';
 import Teams from '../Teams';
 
@@ -29,7 +29,8 @@ describe('Teams', () => {
     });
 
     it('should render spinner while loading', async () => {
-        // TODO - Add code for this test
+        render(<Teams />);
+        expect(screen.getByTestId('spinner')).toBeInTheDocument();
     });
 
     it('should render teams list', async () => {
