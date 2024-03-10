f1908677fca04d07fe8ddd9630fb39b491659792
Luan Gomes <luangms@outlook.com>
2024-03-10 16:59:18 -0300

    Creating global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import {ThemeProvider} from 'styled-components';
 import * as React from 'react';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import TeamOverview from './pages/TeamOverview';
 import Teams from './pages/Teams';
 import UserOverview from './pages/UserOverview';
+import {GlobalStyles} from './styles/global';
+import {defaultTheme} from './styles/themes/default';
 
 const App = () => {
     const router = createBrowserRouter([
@@ -19,7 +22,12 @@ const App = () => {
             element: <UserOverview />,
         },
     ]);
-    return <RouterProvider router={router} />;
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <RouterProvider router={router} />
+            <GlobalStyles />
+        </ThemeProvider>
+    );
 };
 
 export default App;
